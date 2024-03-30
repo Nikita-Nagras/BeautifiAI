215e0be9dad2ca77861966b5c34d31c685c8189c
Gabriel Vasile <gabriel.vasile@email.com>
2024-03-30 20:06:31 +0900

    asd

--- app/services/pebblely_api.rb
+++ app/services/pebblely_api.rb
@@ -5,15 +5,13 @@ class PebblelyApi
   end
 
   def generate_image(cloth_image, theme, prompt)
-
     url = 'https://fashion-api.pebblely.com/api_v1/generate/virtual_pose'
-
     data = File.open(cloth_image).read
     response = HTTParty.post(url, {
       headers: {
         'Content-Type' => 'application/json',
         'Accept' => '*/*',
-        'X-Pebblely-Access-Token' => "#{api_key}"
+        'X-Pebblely-Access-Token' => "#@api_key"
       },
       body: {
         cloth_image: Base64.encode64(data),
@@ -23,9 +21,7 @@ class PebblelyApi
     })
 
     if response.code == 200
-      render json: JSON.parse(response.body), status: :ok
-    else
-      render json: { error: "API request failed: #{response.code} - #{response.message} #{response.body}" }, status: :unprocessable_entity
+      return response.body
     end
   end
 end
