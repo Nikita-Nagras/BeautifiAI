94894fc32d9d7460ad7f23b92708ce93a25c90d4
Nikita Nagras <snbnikita@gmail.com>
2024-03-30 14:56:43 +0900

    basic html view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,38 +1,37 @@
 class PagesController < ApplicationController
-  def index
-  end
 
-  def api_request
-    api_key = ENV['API_KEY']
-    url = 'https://api.stability.ai/v1/generation/stable-diffusion-xl-1024-v1-0/image-to-image'
 
-    response = HTTParty.post(url, {
-      headers: {
-        'Content-Type' => 'multipart/form-data; boundary=<calculated when request is sent>',
-        'Content-Length' => '<calculated when request is sent>',
-        'Host' => '<calculated when request is sent>',
-        'User-Agent' => 'PostmanRuntime/7.37.0',
-        'Accept' => '*/*',
-        'Accept-Encoding' => 'gzip, deflate, br',
-        'Connection' => 'keep-alive',
-        'Authorization' => "Bearer #{api_key}"
-      },
-      body: {
-        init_image: 'value1',
-        init_image_mode: 'IMAGE_STRENGTH',
-        image_strength: '0.35',
-        text_prompts[0][text]: 'send help',
-        cfg_scale: '7',
-        samples: '1',
-        steps: '30'
-      }.to_json
-    })
+  # def api_request
+  #   api_key = ENV['API_KEY']
+  #   url = 'https://api.stability.ai/v1/generation/stable-diffusion-xl-1024-v1-0/image-to-image'
 
-    if response.code == 200
-      render json: JSON.parse(response.body), status: :ok
-    else
-      render json: { error: "API request failed: #{response.code} - #{response.message}" }, status: :unprocessable_entity
-    end
-  end
+  #   response = HTTParty.post(url, {
+  #     headers: {
+  #       'Content-Type' => 'multipart/form-data; boundary=<calculated when request is sent>',
+  #       'Content-Length' => '<calculated when request is sent>',
+  #       'Host' => '<calculated when request is sent>',
+  #       'User-Agent' => 'PostmanRuntime/7.37.0',
+  #       'Accept' => '*/*',
+  #       'Accept-Encoding' => 'gzip, deflate, br',
+  #       'Connection' => 'keep-alive',
+  #       'Authorization' => "Bearer #{api_key}"
+  #     },
+  #     body: {
+  #       init_image: 'value1',
+  #       init_image_mode: 'IMAGE_STRENGTH',
+  #       image_strength: '0.35',
+  #       text_prompts[0][text]: 'send help',
+  #       cfg_scale: '7',
+  #       samples: '1',
+  #       steps: '30'
+  #     }.to_json
+  #   })
+
+  #   if response.code == 200
+  #     render json: JSON.parse(response.body), status: :ok
+  #   else
+  #     render json: { error: "API request failed: #{response.code} - #{response.message}" }, status: :unprocessable_entity
+  #   end
+  # end
 
 end
